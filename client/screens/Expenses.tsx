b014e512e885c621ab6fd837602ea9273ca40eda
izumeeeel <izumi.register@gmail.com>
2021-08-16 14:22:21 +0900

    年後のとの合計取得(wip type)

--- client/screens/Expenses.tsx
+++ client/screens/Expenses.tsx
@@ -1,53 +1,37 @@
 import React from 'react'
 import { ScrollView, StyleSheet, SafeAreaView, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import _ from 'lodash'
 
 import { View } from '../components/Themed'
 import { Row } from '../components/molecules/Row'
 import { useExpenses } from '../hooks/useExpenses'
-
-const years = [
-  {
-    year: 2018,
-    totalExpense: 246000
-  },
-  {
-    year: 2019,
-    totalExpense: 251000
-  },
-  {
-    year: 2020,
-    totalExpense: 229000
-  },
-  {
-    year: 2021,
-    totalExpense: 235000
-  }
-]
+import { IFetchedYearlyExpense } from '../../types/expenses'
 
 const Expenses: React.FC = () => {
   const navigation = useNavigation()
-  const fetchedExpenses = useExpenses()
+  const fetchedExpenses = useExpenses().data
 
   return (
     <>
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
           <>
-            {years.map((year) => (
-              <View key={year.year}>
-                <Row
-                  leftText={year.year.toString() + '年'}
-                  rightText={`¥${year.totalExpense}`}
-                  onPress={() =>
-                    navigation.navigate('ExpensesMonths', {
-                      screen: 'Expenses',
-                      year: year.year
-                    })
-                  }
-                />
-              </View>
-            ))}
+            {fetchedExpenses?.data &&
+              fetchedExpenses.data.map((year) => (
+                <View key={year.year}>
+                  <Row
+                    leftText={year.year.toString() + '年'}
+                    rightText={`¥${year.totalExpense}`}
+                    onPress={() =>
+                      navigation.navigate('ExpensesMonths', {
+                        screen: 'Expenses',
+                        year: year.year
+                      })
+                    }
+                  />
+                </View>
+              ))}
             <Text>{JSON.stringify(fetchedExpenses)}</Text>
           </>
         </ScrollView>
